f92496619f1aa1deafa76317ac1cf226a4c67ded
quangdai <dqdbs06@gmail.com>
2025-05-09 21:40:27 +0700

    Delete Script/navbar.js

--- Script/navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.querySelector(".menu-icon");
-    const menuList = document.querySelector(".menu-list");
-
-    menuIcon.addEventListener("click", function () {
-        menuList.style.display = menuList.style.display === "block" ? "none" : "block";
-    });
-});
