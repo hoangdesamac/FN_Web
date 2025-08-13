39c58efc587413e7da2f70f35ac42da886f37876
FE_JAVA <daidqbs06@gmail.com>
2025-08-13 16:26:49 +0700

    update master product!!

--- docs/Script/js_resetproduct.js
+++ docs/Script/js_resetproduct.js
@@ -629,7 +629,7 @@ function bindEventHandlers() {
 }
 
 // ==========================
-// MODULE: Subtotal
+// MODULE: Subtotals
 // ==========================
 function updateSubtotal() {
     let subtotal = 0;
